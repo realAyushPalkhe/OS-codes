011c52198d186cdc406b530336d7e8baa62d4bd2
realAyushPalkhe <ayush.palkhe23@vit.edu>
2024-11-22 01:48:50 +0530

    LabpractiseCPU.cpp

--- LabpractiseCPU.cpp
+++ LabpractiseCPU.cpp
@@ -305,165 +305,94 @@ int main() {
     FCFS(process, n, arrival, burst);
 
     return 0;
-*/
-#include <limits.h>
-#include <stdbool.h>
-#include <stdio.h>
+*/#include <stdio.h>
 
-struct P {
-    int AT, BT, ST[20], WT, FT, TAT, pos;
-};
+void findAvgTimeRoundRobin(int processes[], int n, int bt[], int at[], int quantum) {
+    int wt[n], tat[n], remaining_time[n];
+    int total_wt = 0, total_tat = 0;
 
-int quant;
-
-void displayGanttChart(int n, struct P p[]) {
-    printf("\nGantt Chart:\n");
-
-    // Print top border of Gantt chart
+    // Initialize remaining time and waiting time
     for (int i = 0; i < n; i++) {
-        int j = 0;
-        while (p[i].ST[j] != -1) {
-            printf("--------");
-            j++;
-        }
-    }
-    printf("\n|");
-
-    // Print process IDs in the Gantt chart
-    for (int i = 0; i < n; i++) {
-        int j = 0;
-        while (p[i].ST[j] != -1) {
-            printf(" P%d |", p[i].pos);
-            j++;
-        }
-    }
-    printf("\n");
-
-    // Print bottom border of Gantt chart
-    for (int i = 0; i < n; i++) {
-        int j = 0;
-        while (p[i].ST[j] != -1) {
-            printf("--------");
-            j++;
-        }
-    }
-    printf("\n");
-
-    // Print timeline below Gantt chart
-    printf("0");
-    for (int i = 0; i < n; i++) {
-        int j = 0;
-        while (p[i].ST[j] != -1) {
-            printf("       %d", p[i].ST[j] + quant <= p[i].FT ? p[i].ST[j] + quant : p[i].FT);
-            j++;
-        }
-    }
-    printf("\n");
-}
-
-int main() {
-    int n, i, j;
-
-    // Taking Input
-    printf("Enter the no. of processes : ");
-    scanf("%d", &n);
-    struct P p[n];
-
-    printf("Enter the quantum: ");
-    scanf("%d", &quant);
-
-    printf("Enter the process numbers:\n");
-    for (i = 0; i < n; i++) scanf("%d", &(p[i].pos));
-
-    printf("Enter the Arrival time of processes:\n");
-    for (i = 0; i < n; i++) scanf("%d", &(p[i].AT));
-
-    printf("Enter the Burst time of processes:\n");
-    for (i = 0; i < n; i++) scanf("%d", &(p[i].BT));
-
-    // Declaring variables
-    int c = n, s[n][20];
-    float time = 0, mini = INT_MAX, b[n], a[n];
-
-    // Initializing burst and arrival time arrays
-    int index = -1;
-    for (i = 0; i < n; i++) {
-        b[i] = p[i].BT;
-        a[i] = p[i].AT;
-        for (j = 0; j < 20; j++) s[i][j] = -1;
+        remaining_time[i] = bt[i]; // Copy burst time to remaining time
+        wt[i] = 0; // Initialize waiting time
     }
 
-    int tot_wt = 0, tot_tat = 0;
-    bool flag = false;
+    int t = 0; // Current time
+    int completed = 0; // Count of completed processes
 
-    // Main Round Robin scheduling loop
-    while (c != 0) {
-        mini = INT_MAX;
-        flag = false;
+    // Process execution loop
+    while (completed < n) {
+        int found = 0; // Flag to check if any process is found
 
-        for (i = 0; i < n; i++) {
-            float p = time + 0.1;
-            if (a[i] <= p && mini > a[i] && b[i] > 0) {
-                index = i;
-                mini = a[i];
-                flag = true;
+        for (int i = 0; i < n; i++) {
+            // If process is arrived and has remaining time
+            if (remaining_time[i] > 0 && at[i] <= t) {
+                found = 1; // A process is found
+
+                if (remaining_time[i] > quantum) {
+                    // Process is not finished
+                    t += quantum; // Increase time by quantum
+                    remaining_time[i] -= quantum; // Decrease remaining time
+                } else {
+                    // Process is finished
+                    t += remaining_time[i]; // Increase time by remaining time
+                    wt[i] = t - at[i] - bt[i]; // Calculate waiting time
+                    remaining_time[i] = 0; // Mark process as completed
+                    completed++; // Increase completed process count
+                }
             }
         }
 
-        if (!flag) {
-            time++;
-            continue;
-        }
-
-        j = 0;
-        while (s[index][j] != -1) j++;
-
-        if (s[index][j] == -1) {
-            s[index][j] = time;
-            p[index].ST[j] = time;
-        }
-
-        if (b[index] <= quant) {
-            time += b[index];
-            b[index] = 0;
-        } else {
-            time += quant;
-            b[index] -= quant;
-        }
-
-        if (b[index] > 0) a[index] = time + 0.1;
-
-        if (b[index] == 0) {
-            c--;
-            p[index].FT = time;
-            p[index].WT = p[index].FT - p[index].AT - p[index].BT;
-            tot_wt += p[index].WT;
-            p[index].TAT = p[index].BT + p[index].WT;
-            tot_tat += p[index].TAT;
+        if (!found) {
+            t++; // If no process is found, increase time
         }
     }
 
-    // Display Gantt Chart
-    displayGanttChart(n, p);
-
-    // Printing process table
-    printf("\nProcess\tArrival Time\tBurst Time\tStart Times\t\tCompletion Time\tWaiting Time\tTurnaround Time\n");
-    for (i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t", p[i].pos, p[i].AT, p[i].BT);
-
-        j = 0;
-        while (s[i][j] != -1) {
-            printf("%d ", p[i].ST[j]);
-            j++;
-        }
-        printf("\t\t%d\t\t%d\t\t%d\n", p[i].FT, p[i].WT, p[i].TAT);
+    // Calculate turnaround time
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
     }
 
-    // Calculating and printing average wait time and turnaround time
-    double avg_wt = tot_wt / (double)n;
-    double avg_tat = tot_tat / (double)n;
-    printf("\nThe average waiting time is: %.2lf\n", avg_wt);
-    printf("The average turnaround time is: %.2lf\n", avg_tat);
+    // Print results
+    printf("Processes   Burst time   Arrival time   Waiting time   Turn around time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i], bt[i], at[i], wt[i], tat[i]);
+    }
+
+    printf("Average waiting time = %.2f\n", (float)total_wt / n);
+    printf("Average turn around time = %.2f\n", (float)total_tat / n);
+}
+
+int main() {
+    int n, quantum;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int processes[n], burst_time[n], arrival_time[n];
+
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1; // Process IDs starting from 1
+    }
+
+    printf("Enter arrival times for each process:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Arrival time for Process %d: ", i + 1);
+        scanf("%d", &arrival_time[i]);
+    }
+
+    printf("Enter burst times for each process:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Burst time for Process %d: ", i + 1);
+        scanf("%d", &burst_time[i]);
+    }
+
+    printf("Enter time quantum: ");
+    scanf("%d", &quantum);
+
+    findAvgTimeRoundRobin(processes, n, burst_time, arrival_time, quantum);
 
     return 0;
 }
