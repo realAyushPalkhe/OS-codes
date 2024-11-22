a25c983f84c4b4cde765145b9692100ebe2e43ce
realAyushPalkhe <ayush.palkhe23@vit.edu>
2024-11-22 09:13:12 +0530

    Labpractisepage.cpp

--- Labpractisepage.cpp
+++ Labpractisepage.cpp
@@ -72,77 +72,99 @@ int main() {
 /*
 LRU:
 
-#include<stdio.h>
+#include <stdio.h>
 
-int lru(int time[],int nf){
-    int i,minimum=time[0],loc=0;
-    for(i=0;i<nf;i++){
-        if(time[i]<minimum){
-            minimum=time[i];
-            loc=i;
+int findLRU(int time[], int n) {
+    int minimum = time[0], pos = 0;
+    for (int i = 1; i < n; i++) {
+        if (time[i] < minimum) {
+            minimum = time[i];
+            pos = i;
         }
     }
-    return loc;
+    return pos;
 }
 
-int main(){
-    int nf,np,frames[10],pages[30],counter=0,time[10];
-    int i,j,flag,faults=0,loc;
-    printf("Enter number of frames: ");
-    scanf("%d",&nf);
-    printf("Enter the number of pages: ");
-    scanf("%d",&np);
-    printf("Enter reference string: ");
-    for(i=0;i<np;i++){
-        scanf("%d",&pages[i]);
+void lruPageReplacement(int pages[], int n, int capacity) {
+    int frames[capacity], time[capacity];
+    int counter = 0, pageFaults = 0, pageHits = 0;
+    
+    // Initialize frames to -1 indicating they are empty
+    for (int i = 0; i < capacity; i++) {
+        frames[i] = -1;
     }
-    for(i=0;i<nf;i++){
-        frames[i]=-1;
-    }
-    for(i=0;i<np;i++){
-        flag=0;
-        for(j=0;j<nf;j++){
-            if(frames[j]==pages[i]){
-                counter++;
-                time[j]=counter;
-                flag=1;
+
+    for (int i = 0; i < n; i++) {
+        int page = pages[i];
+        int found = 0;
+        
+        // Check if the page is already in one of the frames
+        for (int j = 0; j < capacity; j++) {
+            if (frames[j] == page) {
+                found = 1;
+                pageHits++; // Increment page hits if page is already in a frame
+                time[j] = ++counter; // Update the time of access
                 break;
             }
         }
-        if(flag==0){
-            for(j=0;j<nf;j++){
-                if(frames[j]==-1){
-                    counter++;
-                    faults++;
-                    frames[j]=pages[i];
-                    time[j]=counter;
-                    flag=1;
+
+        if (!found) { // Page fault occurs
+            int pos = -1;
+            
+            // Check for an empty frame
+            for (int j = 0; j < capacity; j++) {
+                if (frames[j] == -1) {
+                    pos = j;
                     break;
                 }
             }
-        }
-        if(flag==0){
-            loc=lru(time,nf);
-            counter++;
-            faults++;
-            frames[loc]=pages[i];
-            time[loc]=counter;
-        }
-        printf("\nFrames after accessing page %d: ",pages[i]);
-        for(j=0;j<nf;j++){
-            if(frames[j]!=-1){
-            printf("| %d ",frames[j]);
+
+            // If all frames are filled, find the LRU page to replace
+            if (pos == -1) {
+                pos = findLRU(time, capacity);
             }
-            else{
-                printf("|  ");
+            
+            frames[pos] = page;
+            time[pos] = ++counter; // Update time of access for the replaced page
+            pageFaults++;
+        }
+
+        // Print current frame state
+        printf("Frames: ");
+        for (int j = 0; j < capacity; j++) {
+            if (frames[j] == -1) {
+                printf("- ");
+            } else {
+                printf("%d ", frames[j]);
             }
         }
-        printf("|  ");
-        printf("\n--------------------------");
+        printf("\n");
     }
-    printf("\n Total page faults= %d",faults);
+    
+    printf("Total Page Faults: %d\n", pageFaults);
+    printf("Total Page Hits: %d\n", pageHits);
+}
+
+int main() {
+    int n, capacity;
+
+    printf("Enter the number of pages: ");
+    scanf("%d", &n);
+    
+    int pages[n];
+    printf("Enter the pages: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &pages[i]);
+    }
+
+    printf("Enter the number of frames: ");
+    scanf("%d", &capacity);
+
+    lruPageReplacement(pages, n, capacity);
+
     return 0;
 }
+
 */
 
 /*
